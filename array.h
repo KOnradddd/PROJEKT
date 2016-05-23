cd09f82a77080612b81d07cbd0a901a0079faee6
miton8 <michal.pilch.lote@gmail.com>
2016-05-23 17:44:15 +0200

    Create array.h

---
+++ array.h
@@ -0,0 +1,16 @@
+#ifndef ARRAY_H
+#define ARRAY_H
+
+void m_euklidesowa(double *a, double *b, double *c, int n);
+void m_miejska(double *a, double *b, double *c, int n);
+void m_dyskretna(double *a, double *b, double *c, int n);
+void m_kolejowa(double *a, double *b, double *c, int n);
+void m_xn(double *a, double *b, double *c, int n);
+void m_ham(double *a, double *b, double *c, int n);
+void m_can(double *a, double *b, double *c, int n);
+void m_ssd(double *a, double *b, double *c, int n);
+void m_mae(double *a, double *b, double *c, int n);
+void m_cos(double *a, double *b, double *c, int n);
+void m_bc(double *a, double *b, double *c, int n);
+
+#endif
